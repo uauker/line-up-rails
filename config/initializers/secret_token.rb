5c3eea97384b54963482b25bdbe2fce54108e7c2
Paulo Guilherme <uauker@gmail.com>
2013-07-28 12:27:25 -0300

    arquivos iniciais

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LineUpRails::Application.config.secret_token = '4b8ce56296b4bcda2ba5144a04a598a862f404fc23f994ad453f1dbb40f197c31965de5113c6392f413f07ab99a976624f4a799a58ad128a6d8491ffc320fe4b'
