9f5fc4ab3d49c4b7c2ac9e4c9e51a3988859d99b
Paulo Guilherme <uauker@gmail.com>
2013-08-03 20:25:19 -0300

    incluido friends com POST

--- app/controllers/api/facebook/v1/events/friends_controller.rb
+++ app/controllers/api/facebook/v1/events/friends_controller.rb
@@ -2,8 +2,8 @@ class Api::Facebook::V1::Events::FriendsController < ActionController::Base
   
   respond_to :json
   
-  # curl -H "Content-Type: application/json; charset=UTF-8" -H 'Accept: application/json' --data '{"facebook_users_id":"1,2,3","event_date":"2013-01-31"}' --request GET "http://0.0.0.0:3000/api/facebook/v1/events/friends"  
-  def index
+  # curl -H "Content-Type: application/json; charset=UTF-8" -H 'Accept: application/json' --data '{"facebook_users_id":"1,2,3","event_date":"2013-01-31"}' --request POST "http://0.0.0.0:3000/api/facebook/v1/events/friends"  
+  def create
       friends = params[:facebook_users_id].split(',')
       
       start_date = Date.strptime(params[:event_date], "%Y-%m-%d")
