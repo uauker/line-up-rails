1e9d7f5cc02bc7351f2cae899cb1c033f39120ee
Paulo Guilherme <uauker@gmail.com>
2013-08-03 20:58:00 -0300

    passando os dados para a action de friends

--- app/controllers/api/facebook/v1/events/friends_controller.rb
+++ app/controllers/api/facebook/v1/events/friends_controller.rb
@@ -4,15 +4,17 @@ class Api::Facebook::V1::Events::FriendsController < ActionController::Base
   
   # curl -H "Content-Type: application/json; charset=UTF-8" -H 'Accept: application/json' --data '{"facebook_users_id":"1,2,3","event_date":"2013-01-31"}' --request POST "http://0.0.0.0:3000/api/facebook/v1/events/friends"  
   def create
-      friends = params[:facebook_users_id].split(',')
+      friends = params[:friends]
+    
+      friend_ids = friends[:facebook_users_id].split(',')
       
-      start_date = Date.strptime(params[:event_date], "%Y-%m-%d")
+      start_date = Date.strptime(friends[:event_date], "%Y-%m-%d")
       start_date = start_date.change({:hour => 0, :min => 0, :sec => 0})
       
-      end_date = Date.strptime(params[:event_date], "%Y-%m-%d")
+      end_date = Date.strptime(friends[:event_date], "%Y-%m-%d")
       end_date = end_date.change({:hour => 23, :min => 59, :sec => 59})
 
-      render :json => User.where('facebook_user_id IN (?) AND event_date BETWEEN ? AND ?', friends, start_date, end_date)
+      render :json => User.where('facebook_user_id IN (?) AND event_date BETWEEN ? AND ?', friend_ids, start_date, end_date)
     rescue
       render :json => { :status => 'error' }
   end
