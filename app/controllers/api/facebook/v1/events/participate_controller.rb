65f26ca8a8eece3f5ef669a25f6a9214492d55d4
Paulo Guilherme <uauker@gmail.com>
2013-08-03 20:03:57 -0300

    mostrando itens amigos

--- app/controllers/api/facebook/v1/events/participate_controller.rb
+++ app/controllers/api/facebook/v1/events/participate_controller.rb
@@ -3,7 +3,7 @@ class Api::Facebook::V1::Events::ParticipateController < ActionController::Base
   respond_to :json
   
   def index
-    render :json => { :total => User.all.size }   
+    render :json => User.limit(15)
   end
   
 end
