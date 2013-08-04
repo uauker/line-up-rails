35af314a14fb581256dfb3059d3614fcf44cad8d
Paulo Guilherme <uauker@gmail.com>
2013-08-04 14:38:17 -0300

    tirando da agenda

--- app/controllers/api/facebook/v1/events/unsubscribe_controller.rb
+++ app/controllers/api/facebook/v1/events/unsubscribe_controller.rb
@@ -2,8 +2,8 @@ class Api::Facebook::V1::Events::UnsubscribeController < ActionController::Base
   
   respond_to :json
   
-  # curl -H "Content-Type: application/json; charset=UTF-8" -H 'Accept: application/json' --data '{"facebook_user_id":"1","event_date":"2013-01-31"}' --request GET "http://0.0.0.0:3000/api/facebook/v1/events/unsubscribe"  
-  def index
+  # curl -H "Content-Type: application/json; charset=UTF-8" -H 'Accept: application/json' --data '{"facebook_user_id":"1","event_date":"2013-01-31"}' --request POST "http://0.0.0.0:3000/api/facebook/v1/events/unsubscribe"  
+  def create
       unsubscribe = params[:unsubscribe]
       
       date = Date.strptime(unsubscribe[:event_date], "%Y-%m-%d")
